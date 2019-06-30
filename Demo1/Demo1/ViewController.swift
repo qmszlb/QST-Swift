f4b036bfefc81087d947064159cb0b6cdcd1ce55
qmszlb <36808182+qmszlb@users.noreply.github.com>
2019-06-30 14:47:17 +0800

    Add files via upload
    简单实现了页面跳转

---
+++ Demo1/Demo1/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Demo1
+//
+//  Created by 张伟 on 2019/6/30.
+//  Copyright © 2019年 张伟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
